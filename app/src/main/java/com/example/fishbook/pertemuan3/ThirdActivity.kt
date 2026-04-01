dea407acee805570bde3be77a5433f6abbce3e4a
lab151-33 <miftahulrizki204@gmail.com>
2026-04-01 16:38:29 +0700

    Commit Mobile Day 3

---
+++ app/src/main/java/com/example/fishbook/pertemuan3/ThirdActivity.kt
@@ -0,0 +1,42 @@
+package com.example.fishbook.pertemuan3
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.fishbook.R
+import com.example.fishbook.databinding.ActivityThirdBinding
+
+class ThirdActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityThirdBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        //setContentView(R.layout.activity_third)
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // Inisialisasi komponen
+        //val inputNama: EditText = findViewById(R.id.inputNoTujuan)
+        //val btnSubmit: Button = findViewById(R.id.btnKirim)
+
+        binding.btnKirim.setOnClickListener {
+            //Mengambil value dari inputNama dan menampilkan di Logcat
+            val no = binding.inputNoTujuan.text
+
+            Toast.makeText(this, "Kode berhasil dikirim ke $no", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, ThirdResultActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
